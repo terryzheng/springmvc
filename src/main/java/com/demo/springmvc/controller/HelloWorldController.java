9bdb73df3b18593bb8abfbf94d788c81c98df98a
terryzheng <zl_terry@126.com>
2015-04-14 16:56:52 +0800

    add rabbitMQ consumer to this demo

--- src/main/java/com/demo/springmvc/controller/HelloWorldController.java
+++ src/main/java/com/demo/springmvc/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.demo.springmvc.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.demo.springmvc.repository.UserRepository;
+import com.demo.springmvc.service.ProducerMq;
 
 @Controller
 public class HelloWorldController {
@@ -17,11 +20,17 @@ public class HelloWorldController {
 	@Value("#{config['jdbc.driverClass']}")
 	private String driverClass;
 
+	@Resource
+	private ProducerMq producer;
+
 	@RequestMapping("/hello")
 	public String hello(
 			@RequestParam(value = "corpId", required = false, defaultValue = "79941768") String corpId,
 			Model model) {
 		String corpName = userRepository.getUserStatusByCorpId(corpId);
+		for (int i = 0; i < 100; i++) {
+			producer.sendDataToCrQueue("data" + i);
+		}
 		model.addAttribute("name", driverClass + corpName);
 		return "helloworld";
 	}
