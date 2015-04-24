1f0d7058a727138a19cbf39032303016c4437aed
terryzheng <zl_terry@126.com>
2015-04-24 17:31:35 +0800

    add producer jar

--- src/main/java/com/demo/springmvc/controller/HelloWorldController.java
+++ src/main/java/com/demo/springmvc/controller/HelloWorldController.java
@@ -1,7 +1,5 @@
 package com.demo.springmvc.controller;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
