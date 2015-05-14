eb2fa65ef06ffd68ef02a55e96ad5c312973200f
terryzheng <zl_terry@126.com>
2015-05-14 17:23:46 +0800

    fix

--- src/main/java/com/demo/springmvc/controller/HelloWorldController.java
+++ src/main/java/com/demo/springmvc/controller/HelloWorldController.java
@@ -1,14 +1,5 @@
 package com.demo.springmvc.controller;
 
-import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
-
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.Watcher.Event.KeeperState;
-import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.data.Stat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -16,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.alibaba.fastjson.JSONObject;
 import com.demo.springmvc.repository.UserRepository;
 import com.zhaopin.rabbitMQ.producer.ProducerMq;
 
@@ -30,41 +22,20 @@ public class HelloWorldController {
 	@Autowired
 	private ProducerMq producer;
 
-	private ZooKeeper zooKeeper;
-	private CountDownLatch connectedSignal = new CountDownLatch(1);
-
 	@RequestMapping("/hello")
 	public String hello(
 			@RequestParam(value = "corpId", required = false, defaultValue = "79941768") String corpId,
-			Model model) throws IOException, KeeperException,
-			InterruptedException {
+			Model model) {
 		String corpName = userRepository.getUserStatusByCorpId(corpId);
-		zooKeeper = new ZooKeeper("172.17.0.119:2181", 5000, new ConnWatcher());
-		System.out.println(zooKeeper.exists("/hrc/queuelist", true));
-		System.out.println(zooKeeper
-				.getData("/hrc/queuelist", true, new Stat()));
-		System.out.println(zooKeeper.setData("hrc", "555".getBytes(), 2));
+		for (int i = 0; i < 10000; i++) {
+			JSONObject object = new JSONObject();
+			object.put("key", "key" + i);
+			object.put("value", "value" + i);
+			object.put("ttl", i);
+			producer.convertAndSend("redistest", object);
+		}
 		model.addAttribute("name", driverClass + corpName);
 		return "helloworld";
 	}
 
-	public ZooKeeper getZooKeeper() {
-		return zooKeeper;
-	}
-
-	public void setZooKeeper(ZooKeeper zooKeeper) {
-		this.zooKeeper = zooKeeper;
-	}
-
-	public class ConnWatcher implements Watcher {
-		@Override
-		public void process(WatchedEvent event) {
-			// 连接建立, 回调process接口时, 其event.getState()为KeeperState.SyncConnected
-			if (event.getState() == KeeperState.SyncConnected) {
-				// 放开闸门, wait在connect方法上的线程将被唤醒
-				connectedSignal.countDown();
-			}
-		}
-
-	}
 }
